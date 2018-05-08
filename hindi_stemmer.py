e26b84d0e9528e4173188cb31b466eb0ea6fb8b9
Akshay Sharma <akshayaxe2096@gmail.com>
2018-05-09 02:37:39 +0530

    Update hindi_stemmer.py

--- hindi_stemmer.py
+++ hindi_stemmer.py
@@ -4,43 +4,32 @@ filename = "test_dataset.txt"
 with codecs.open(filename, encoding='utf-8') as f:
     txt = f.read()
 f.close()
-file = open("out.txt",'w', encoding='utf-8')
+file = open("output.txt",'w', encoding='utf-8')
 
-''' testing '''
-# file_maatra = open("maatra.txt",'w', encoding='utf-8')
-# file_temp = open("temp.txt", 'w', encoding = 'utf-8')
-# print(len(txt))
-
-maatra = ["ि", "ा", "ी", "े", "ु", "ू", "ँ", "ं", "ो"]
-
-''' Copy test_dataset to testing_dataset to avoid unwanted'''
-fileout = open("testing_dataset.txt", 'w', encoding = 'utf-8')
-for i in range(len(txt)):
-    fileout.write(txt[i])
-with codecs.open("testing_dataset.txt", encoding='utf-8') as f:
-    txt = f.read()
-# print(len(txt))
+maatra = ["ि", "ा", "ी", "े", "ु", "ू", "ँ", "ं", "ो", "ि◌या", "ि◌यो", "◌ाए"]
 
 ''' Stemming '''
-len_of_word = 0
-bytes_read = 0
-for i in range(0,len(txt)):
-    j = 0
-    # file_temp.write("txt[" + str(i) +"] : " + txt[i] + "\n")
-    if txt[i] == "\n":
-        len_of_word = len_of_word-1
-        # print("bytes_read : ", i)
-        # print("len_of_word : ", len_of_word)
-        # for j in range(i-2, (i-2)-len_of_word, -1):
-        #     file_maatra.write("txt[" + str(j) +"] : " + txt[j] + "\n")
-        for j in range(i-2, (i-2)-len_of_word, -1):
-            if txt[j] not in maatra:
-                break
-        # print(j)
-        for k in range((i-2)-len_of_word+1, j+1):
-            file.write(txt[k])
-        file.write("\n")
-        len_of_word = 0
-    else:
-        len_of_word = len_of_word+1
-
+to_remove = ['\ufeff', '\r']
+i=0
+stems=[]
+while(i<len(txt)):
+    a = []
+    while(txt[i]!='\n'):
+        a.append(txt[i])
+        i=i+1
+    a = [k for k in a if k not in to_remove]
+    a= ''.join(a)
+    b = a
+    file.write(b + " : ")
+    k = 0
+    while(k<len(maatra)):
+        x = a.rfind(maatra[k])
+        k = k+1
+        if x == len(a)-1:
+            a = a[:x]
+            k = 0
+    file.write(a)
+    file.write("\n")
+    if (txt[i] == '\n'):
+        i = i+1
+file.close()
